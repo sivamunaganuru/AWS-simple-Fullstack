dd3f4be056ff7682788f3fb06bf0b9332fd0979f
Siva Munaganuru <smunagan@asu.edu>
2024-04-06 02:04:35 -0700

    updated

--- frontend/apiurl.sh
+++
@@ -1,2 +0,0 @@
-API_URL=$(aws cloudformation describe-stacks --stack-name YourStackName --query "Stacks[0].Outputs[?OutputKey=='ApiEndpoint'].OutputValue" --output text)
-echo "REACT_APP_API_URL=${API_URL}" > .env
